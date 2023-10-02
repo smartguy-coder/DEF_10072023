dd9b6ccf7b727f0f770b8cfb2389d79435062715
Василь Картичак <w.i.k.mailua@gmail.com>
2023-10-02 19:50:19 +0300

    17

--- Makefile
+++ Makefile
@@ -7,5 +7,11 @@ su:
 newapp:
 	docker compose run --rm web-app sh -c "python manage.py startapp $(app)"
 
+migrations:
+	docker compose run --rm web-app sh -c "python manage.py makemigrations"
+
+migrate:
+	docker compose run --rm web-app sh -c "python manage.py migrate"
+
 from_file:
 	@sh from_file.sh
--- library/main/models.py
+++ library/main/models.py
@@ -1,9 +1,19 @@
 from decimal import Decimal
 
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Book(models.Model):
     title = models.CharField(max_length=50, null=False)
     price = models.DecimalField(max_length=8, decimal_places=2, default=Decimal('1.00'),
-                                )
+                                validators=[MinValueValidator(1)])
+    created_at = models.DateTimeField(auto_created=True, auto_now=True)
+    last_change_at = models.DateTimeField(auto_now_add=True)
+    pages = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.title} - {self.pages}'
+
+    def __len__(self):
+        return self.pages
