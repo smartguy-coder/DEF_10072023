9c413eef804a1d9b0d057a99333b87191d8da828
Василь Картичак <w.i.k.mailua@gmail.com>
2023-10-12 20:03:07 +0300

    30

--- library/library/urls.py
+++ library/library/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from main import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', admin.site.urls),
+    path('', views.BookListView.as_view()),
 
 ]
