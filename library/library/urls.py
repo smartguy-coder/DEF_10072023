2655c40547dbcbdb01ad18df88a7fb4f98469ffd
Василь Картичак <w.i.k.mailua@gmail.com>
2023-10-16 18:39:38 +0300

    34

--- library/library/urls.py
+++ library/library/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.BookListView.as_view()),
     path('book/add/', views.BookCreateView.as_view()),
+    path('book/<int:pk>/edit/', views.BookEditView.as_view()),
     path('func/', views.books_getter),
 
 ]
