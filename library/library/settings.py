ab760c4b04f63e22b5a0687232a3fdef7f4ee227
Василь Картичак <w.i.k.mailua@gmail.com>
2023-10-23 20:08:12 +0300

    55

--- library/library/settings.py
+++ library/library/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     "debug_toolbar",
+    "cachalot",
 
     'main',
     'second',
@@ -129,3 +130,10 @@ if DEBUG:
     import socket  # only if you haven't already imported this
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://redis:6379",
+    }
+}
